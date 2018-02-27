e2405aec25a86417c2d07742e0bffde12b1060e6
华锦念 <2430960358@qq.com>
2018-02-27 13:23:17 +0800

    updata

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'haha' });
+  res.render('index', { title: 'nicai' });
 });
 
 module.exports = router;
